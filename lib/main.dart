c2b6664aa97fe75ea6802fe21651c305058bd1a3
Abubakar <Gwabstech@gmail.com>
2022-05-07 13:22:00 +0100

    Elivated button

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'what/s my name',
+      'how old am i',
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First Flutter App'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('My First Flutter App'),
+          ),
+          body: Column(
+            children: const [
+              Text('the Q'),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('answer 1'),
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('answer 1'),
+              ),
+            ],
+          )),
     );
   }
 }
